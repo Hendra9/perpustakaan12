e779858480f6428256851cec77f2573980c8e540
Hendra9 <30978127+Hendra9@users.noreply.github.com>
2018-05-07 21:38:15 +0700

    Add files via upload

---
+++ upload_gambar.php
@@ -0,0 +1,29 @@
+<?php
+include("conn/koneksi.php");
+$message = '';
+if (isset($_POST['upload'])) {
+	$keterangan = $_POST['keterangan'];
+	require_once('proses_upload.php');
+}
+?>
+
+<html>
+<head>
+	<title>Upload Gambar Sederhana by KangSunu</title>
+</head>
+<body>
+<?php echo $message; ?>
+
+<form action="" method="post" enctype="multipart/form-data" accept="image/jpg">
+	<label>Pilih gambar</label>
+	<input type="file" name="photo" required>
+	<br>
+	<label>Keterangan</label>
+	<textarea type="text" name="keterangan"></textarea>
+	<br>
+	<button type="submit" name="upload">Upload</button>
+</form>
+
+
+</body>
+</html>
